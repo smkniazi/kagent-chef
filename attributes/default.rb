e09c7c758a9717c9866d0e79959c9380a6c3e9d4
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2018-10-24 23:51:28 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ default["install"]["ssl"]                          = "false"
 default["install"]["cleanup_downloads"]            = "false"
 default["install"]["upgrade"]                      = "false"
 default["install"]["addhost"]                      = "false"
-default["install"]["version"]                      = "0.6.0-SNAPSHOT"
+default["install"]["version"]                      = "0.6.0"
 default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0"
 
 # Default values for configuration parameters
