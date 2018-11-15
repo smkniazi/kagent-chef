80da6e41199b80b5df95f29687a95003ff6ee298
Jim Dowling <jdowling@sics.se>
2018-11-15 18:56:50 +0100

    [HOPSWORKS-811] Lazy Conda Environment Creation

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,8 +4,8 @@ default["install"]["ssl"]                          = "false"
 default["install"]["cleanup_downloads"]            = "false"
 default["install"]["upgrade"]                      = "false"
 default["install"]["addhost"]                      = "false"
-default["install"]["version"]                      = "0.6.0"
-default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0"
+default["install"]["version"]                      = "0.6.1"
+default["install"]["versions"]                     = "0.1.0,0.2.0,0.3.0,0.4.0,0.4.1,0.4.2,0.5.0,0.6.0"
 
 # hops-util-py version, when value is "master" install from git, when value is "0.6.0.0" do pip install hops==0.6.0.0
 default["kagent"]["hops-util-py-version"]          = "0.6.0.1"
